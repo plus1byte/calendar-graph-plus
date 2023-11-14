0373e361b506d0aa8e205820b28b4dd577c13268
+1 <t@ttt.tl>
2023-11-14 07:47:38 +0000

    chore: build by rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+
+const pkg = require('./package.json')
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      name: 'calendar-graph-plus',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    resolve({ extensions: ['.js'] }),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx'],
+      exclude: 'node_modules/',
+    }),
+    uglify(),
+  ],
+}
